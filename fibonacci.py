bd234fab5ea340b2feb464e660baa01462afdd6a
santosst3 <73975120+santosst3@users.noreply.github.com>
2022-09-30 09:36:08 -0300

    New version: fibonacci with recursive algorithm

--- fibonacci.py
+++ fibonacci.py
@@ -1,22 +1,23 @@
+def calc_fibo(fiboclass):
+    fiboclass.n_current = fiboclass.calc_fibo_recursive(fib1.n_max)
+
+
 class fibonacci_number:
     def __init__(self, nmax):
-        self.n_ant = 0
         self.n_current = 1
         self.n_max = nmax
-        self.n = 2
         if self.n_max <= 1:
             self.n_current = 0
 
     def __repr__(self):
         return f'fibonacci number of {self.n_max}: {self.n_current}'
 
-    def calc_fibo(self):
-        while self.n < self.n_max:
-            aux1 = self.n_current
-            self.n_current += self.n_ant
-            self.n_ant = aux1
-            self.n += 1
+    def calc_fibo_recursive(self, n):
+        if n > 1:
+            return self.calc_fibo_recursive(n-1) \
+                + self.calc_fibo_recursive(n-2)
+        return n
 
 
-fib1 = fibonacci_number(1)
-fib1.calc_fibo()
+fib1 = fibonacci_number(5)
+calc_fibo(fib1)
